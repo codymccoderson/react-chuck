28ccfac6f3234efab1532b4308d6c884d90c423a
Peter Jones <peteflux@gmail.com>
2020-03-24 21:09:29 -0400

    mount included

--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -5,7 +5,15 @@ class Quote extends Component {
     state = {
         quote: "Fetching a quote..."
     }
-    
+
+    componentDidMount() {
+        setTimeout (() => {
+            this.setState({
+                quote: "Chuck Norris does not care about your rules."
+            })
+        }, 2000);
+    }
+
     render() {
         const { quote } = this.state;
         return (
